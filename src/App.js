928c8ab982d3eb544f900e292a7ed850b7a72d36
patricia-diasr <patricia.d.rodrigues345@gmail.com>
2024-03-06 20:59:40 -0300

    Listagem de tasks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
+import { useState, useEffect } from 'react';
+
 import Container from "./components/layout/Container";
 import Toolbar from "./components/layout/Toolbar";
 import Todo from "./components/todo/Todo";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect( () => {
+    fetch("http://localhost:5000/tasks", {
+        method: "GET",
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    .then( resp => resp.json() )
+    .then ( data => {
+        setTasks(data);
+        setLoading(false);
+    } )
+    .catch( err => console.log(err) );
+  }, []);
+
   return (
     <div className="App">
       <Toolbar />
       <Container>
-        <Todo />
+        { tasks.length > 0 &&
+          tasks.map((task) => (
+            <Todo 
+              id={task.id}
+              name={task.name}
+              category={task.category}
+              description={task.description}
+              date={task.date}
+              key={task.id}
+              // handleRemove={removeTask}
+            />
+          ))
+        }
       </Container>
     </div>
   );
--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,17 +1,17 @@
 import styles from './Todo.module.css'
 import { MdOutlineBookmarkBorder, MdModeEditOutline, MdDelete } from "react-icons/md";
 
-function Todo() {
+function Todo({ id, name, category, description, date }) {
     return (
         <main className={styles.task}>
             <div className={styles.first_line}>
-                <h2>Prova B</h2>
+                <h2>{name}</h2>
                 <MdOutlineBookmarkBorder />
             </div>
-            <p className={styles.category}>Categoria</p>
-            <p className={styles.description}>Nunc quam velit, viverra eget turpis ut, ullamcorper egestas lorem. Suspendisse consequat scelerisque condimentum. Curabitur velit erat, sollicitudin sit amet malesuada in, tincidunt tincidunt risus. Ut vitae vulputate augue. Fusce egestas nulla lacus, vel interdum leo sodales vitae. Suspendisse potenti. Vivamus nec egestas mi. Suspendisse potenti. Vivamus nec egestas mi. Suspendisse potenti. Vivamus nec egestas mi. Vivamus nec egestas mi.</p>
+            <p className={styles.category}>{category}</p>
+            <p className={styles.description}>{description}</p>
             <div className={styles.last_line}>
-                <p>Data: 10/05/22</p>
+                <p>Data: {date}</p>
                 <div>
                     <button><MdModeEditOutline /></button>
                     <button><MdDelete /></button>
